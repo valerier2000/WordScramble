4e8b3002428634eae643e7f164cf91f36a9d5fcd
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-04-11 16:59:31 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Valeriia Rohatynska on 11/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
